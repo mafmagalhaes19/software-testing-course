6c8105d9348f76c040b3b9abab07a248877ad3e0
mafmagalhaes19 <mafi.magalhaes@gmail.com>
2022-10-13 02:45:55 +0100

    Finished chapter 3 Testing Customer Registration Feature

--- src/main/java/com/amigoscode/testing/customer/CustomerRegistrationService.java
+++ src/main/java/com/amigoscode/testing/customer/CustomerRegistrationService.java
@@ -3,6 +3,10 @@ package com.amigoscode.testing.customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Iterator;
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 public class CustomerRegistrationService {
 
@@ -14,6 +18,28 @@ public class CustomerRegistrationService {
     }
 
     public void registerNewCustomer(CustomerRegistrationRequest request){
+        // 1. PhoneNumber is taken
+        // 2. If taken, let's check if it belongs to same customer
+        // 2.1. If yes, return
+        // 2.2. Throw an exception
+        // 3. Save customer
 
+        String phoneNumber = request.getCustomer().getPhoneNumber();
+        Optional<Customer> optional = customerRepository.selectCustomerByPhoneNumber(phoneNumber);
+
+        if (optional.isPresent()) {
+            Customer customer = optional.get();
+            if (customer.getName().equals(request.getCustomer().getName())) {
+                System.out.println("The customer is already registered.");
+                return;
+            }
+            throw new IllegalStateException(String.format("This phone number [%s] is already taken.", phoneNumber));
+        }
+
+        if(request.getCustomer().getId() == null) {
+            request.getCustomer().setId(UUID.randomUUID());
+        }
+
+        customerRepository.save(request.getCustomer());
     }
 }
--- src/test/java/com/amigoscode/testing/customer/CustomerRepositoryTest.java
+++ src/test/java/com/amigoscode/testing/customer/CustomerRepositoryTest.java
@@ -3,13 +3,15 @@ package com.amigoscode.testing.customer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@DataJpaTest
+@DataJpaTest(properties = {"spring.jpa.properties.javax.persistence.validation.mode=none"})
 class CustomerRepositoryTest {
 
     @Autowired
@@ -29,19 +31,98 @@ class CustomerRepositoryTest {
         assertThat(optionalCustomer)
                 .isPresent()
                 .hasValueSatisfying( c -> {
-//                    assertThat(c.getId()).isEqualTo(id);
-//                    assertThat(c.getName()).isEqualTo("Abel");
-//                    assertThat(c.getPhoneNumber()).isEqualTo("0000");
                     assertThat(c).isEqualToComparingFieldByField(customer);
                 });
 
     }
 
     @Test
+    void itNotShouldSelectCustomerByPhoneNumberWhenNumberDoesNotExist() {
+        //Given
+        String phoneNumber = "0000";
+
+        //When -> action
+        Optional<Customer> optionalCustomer = underTest.selectCustomerByPhoneNumber(phoneNumber);
+
+        //Then
+        assertThat(optionalCustomer).isNotPresent();
+
+    }
+
+    @Test
     void itShouldSaveCustomer() {
         //Given
+        UUID id = UUID.randomUUID();
+        Customer customer = new Customer(id, "Abel", "0000");
+        //When
+        underTest.save(customer);
+        //Then
+        Optional<Customer> optionalCustomer = underTest.findById(id);
+        assertThat(optionalCustomer)
+                .isPresent()
+                .hasValueSatisfying(c -> {
+//                    assertThat(c.getId()).isEqualTo(id);
+//                    assertThat(c.getName()).isEqualTo("Abel");
+//                    assertThat(c.getPhoneNumber()).isEqualTo("0000");
+                    assertThat(c).isEqualToComparingFieldByField(customer);
+                });
+    }
+
+    @Test
+    void itShouldNotSaveCustomerWhenNameIsNull() {
+        // Given
+        UUID id = UUID.randomUUID();
+        Customer customer = new Customer(id, null, "0000");
+
+        // When
+        // Then
+        assertThatThrownBy(() -> underTest.save(customer))
+                .hasMessageContaining("not-null property references a null or transient value : com.amigoscode.testing.customer.Customer.name")
+                .isInstanceOf(DataIntegrityViolationException.class);
+
+    }
+
+    @Test
+    void itShouldNotSaveCustomerWhenPhoneNumberIsNull() {
+        // Given
+        UUID id = UUID.randomUUID();
+        Customer customer = new Customer(id, "Alex", null);
+
+        // When
+        // Then
+        assertThatThrownBy(() -> underTest.save(customer))
+                .hasMessageContaining("not-null property references a null or transient value : com.amigoscode.testing.customer.Customer.phoneNumber")
+                .isInstanceOf(DataIntegrityViolationException.class);
+
+    }
+
+    @Test
+    void itShouldSelectCustomerByPhoneNUmber() {
+        //Given
+        UUID id = UUID.randomUUID();
+        Customer customer = new Customer(id, "Abel", "0000");
 
         //When
+        underTest.save(customer);
+
         //Then
+        Optional<Customer> optionalCustomer = underTest.selectCustomerByPhoneNumber(("0000"));
+        assertThat(optionalCustomer)
+                .isPresent()
+                .hasValueSatisfying((c -> {
+                    assertThat(c).isEqualToComparingFieldByField(customer);
+                }));
+    }
+
+    @Test
+    void itNotShouldSelectCustomerByPhoneNumberWhenNumberDoesNotExists() {
+        // Given
+        String phoneNumber = "0000";
+
+        // When
+        Optional<Customer> optionalCustomer = underTest.selectCustomerByPhoneNumber(phoneNumber);
+
+        // Then
+        assertThat(optionalCustomer).isNotPresent();
     }
 }
